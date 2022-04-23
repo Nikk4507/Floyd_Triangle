d10fa7e500f92b5cf15a79db38d83b05999dbe03
NIKHIL VERMA <99395398+Nikk4507@users.noreply.github.com>
2022-04-23 19:06:14 +0530

    Update floyd_triangle.cpp

--- floyd_triangle.cpp
+++ floyd_triangle.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 int main()
 {
-    int i,j,n,count=1;
-    cout<<"Enter the n: ";
-    cin>>n;
+    int i,j,number,count=1;
+    cout<<"Enter the number upto you want: ";
+    cin>>number;
 
     for(i=1;i<=n;i++){
         for(j=1;j<=i;j++){
